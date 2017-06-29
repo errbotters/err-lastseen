71677093852c9c2c1902698a558d62ca91782edf
JakubNvk <jakub.novak@gmail.com>
2017-06-29 21:04:59 +0200

    Check when user was last seen in irc chat.
    * User can now check when another user was last seen in irc chat.
    * Plugin automatically updates time whenever user posts something to chat.
    
    Signed-off-by: JakubNvk <jakub.novak@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # err-lastseen
+A plugin for Errbot which tells you when user was last seen in irc chat.
---
+++ lastseen.py
@@ -0,0 +1,48 @@
+import re
+import sys
+from datetime import datetime
+from errbot import BotPlugin, botcmd, re_botcmd
+
+
+USAGE_STR = 'Nick not specified. (usage: !last_seen <nick>)'
+
+
+class LastSeen(BotPlugin):
+    def activate(self):
+        super(LastSeen, self).activate()
+        try:
+            if type(self['last_seen']) is not dict:
+                self['last_seen'] = {}
+        except KeyError:
+            self['last_seen'] = {}
+
+
+    @re_botcmd(pattern=r'^', prefixed=False, flags=re.IGNORECASE)
+    def update_last_seen(self, msg, match):
+        user = str(msg.frm).split('!')[0]
+        time = datetime.now()
+
+        last_seens = self['last_seen']
+        last_seens[user] = time
+        self['last_seen'] = last_seens
+
+
+    @botcmd
+    def last_seen(self, msg, args):
+        """Return last activity and its time of given user.
+
+        Examples:
+            !last_seen <user>
+        """
+        last_seens = self['last_seen']
+
+        if len(args) < 1:
+            return USAGE_STR
+        user = args.split(' ')[0]
+
+        try:
+            when = last_seens[user].strftime("%d-%m-%Y %H:%M")
+        except KeyError:    
+            return 'We have not seen {} yet.'.format(user)
+
+        return '{} was last seen on {}.'.format(user, when)
